419bbd56af3d8099d798fd17d9a3db5ba0530ea2
Dattlee <Dattlee@Davids-MacBook-Pro.local>
2017-03-02 03:08:35 +0000

    Add resourceProbem and tournament algorightm (microbial GA)

--- AIAB/Lab4/resourceProblem.m
+++ AIAB/Lab4/resourceProblem.m
@@ -6,26 +6,19 @@ B = [6    19     7     1    13    17     3    18    16    20     5    15    12  
 %volume
 V = [10     5    15    18     1    17    12    19    16     4     9    20    13     7     3     2     6    11     8    14];
 
-% tournament(B,V,maxVol,pop,gen)
-gen = 2000; % number of generations / number of matchups
 
-[winner,winnerInd,fitRec,popGens] = tournament(B,V,50,100,gen);
+gen = 4000; % number of generations / number of matchups
+pop = 100;
+local = 5;
+cross = 0.5;
 
+[winner,winnerInd,fitRec,popGens] = tournament(B,V,Max_Volume,pop,gen,local,cross);
 
-x = linspace(1,gen,gen+1);
+x = linspace(1,gen,gen);
+figure;plot(x,fitRec');
 
-plot(x,fitRec');
-
-while winner<111
-    [winner,winnerInd,fitRec,popGens] = tournament(B,V,50,100,gen);
-end
-
-
-
-% % sourced from Stack over flow: http://stackoverflow.com/questions/2692482/get-the-indices-of-the-n-largest-elements-in-a-matrix
-[sortedValues,sortIndex] = sort(fitRec(:),'descend');  %# Sort the values in
-maxIndex = sortIndex(1:4);                             %#   descending order retrun top 4 indices
-
+[sortedValues,sortIndex] = sort(fitRec(:,end),'descend');  % Sort the values in
+maxIndex = sortIndex(1:4);                             %   descending order retrun top 4 indices
 
 solutions = popGens(maxIndex,:);
 save('solutions.mat','solutions');
--- AIAB/Lab4/tournament.m
+++ AIAB/Lab4/tournament.m
@@ -1,51 +1,66 @@
-function [winner,winnerInd,fitRec,popGens] = tournament(B,V,maxVol,pop,gen)
+function [winner,winnerInd,fitRec,popGens] = tournament(B,V,maxVol,pop,gen,local,cross)
 % B = benefit of each gene
 % V = volume of each gene
 % maxVol = maximum volume of the algorithm
 % pop = size of population
-% gen = number of generations/matchups
+% gen = number of generations/matchups. The Algorithm considers the first
+%       initialised generation as part of the 'gen' generations
+% local = size of the local/neighbourhood search space for a competitor
+% cross = crossover probability of genes
 
-    popGens = randi([0 1],pop,length(B));
-    popVols = popGens*V';
-    popFits = popGens*B'.*(popVols<=maxVol);
-    local = 10; % size local search space when picking matchup
-    cross = 0.5; % percentage chance of crossover
-    mutRate = 1/length(B); % mutation rate
-    
-    fitRec = zeros(pop,gen+1);
+    popGens = randi([0 1],pop,length(B)); % Initialise a random population of
+                                          % 'pop' individuals
+    popVols = popGens*V'; % Calculate the volume of each phenotype
+    popFits = popGens*B'.*(popVols<=maxVol);  % calculate the the fitness of each
+                                              % phenotype and set the fitness to 0
+                                              % if its volume is greater than maxVol
+    mutRate = 1/length(B);  % mutation rate ** Low enough so that on average
+                            % only one gene mutates after evaluation
+
+    fitRec = zeros(pop,gen);
     fitRec(:,1) = popFits;
 
-    for g = 1:gen;
-        A = randi(pop);
-        C = mod((A + randi(local)),pop) + 1;
-        
-        if popFits(A) > popFits(C)
-            w = A; % Winner
-            l = C; % Loser
+    for g = 2:gen;
+
+        % Select two individuals at random
+        first = randi(pop);
+        % Select a competitor from Deme *** Note: local search space maintains diversity
+        second = mod((first + randi(local)),pop) + 1; % mod function wraps around
+                                                  % +1 prevents 0 index
+
+        % Evaluate strongest individual
+        if popFits(first) > popFits(second);
+            w = first; % Winner
+            l = second; % Loser
         else
-            w = C; % Winner
-            l = A; % Loser
+            w = second; % Winner
+            l = first; % Loser
         end
-            %random chance of recieving each gene
-        for allele = 1:length(B)
-            if rand() < cross
+
+
+        % Mutation of the Loser
+        for allele = 1:length(B);
+            % Random crossover of each gene
+            if rand() < cross;
                 popGens(l,allele) = popGens(w,allele);
             end
+            % Random mutation of an allele
             if rand() < mutRate
                 popGens(l,allele) = ~popGens(l,allele);
             end
         end
-        
+
+        % Update volume of Loser
         popVols(l) = popGens(l,:)*V';
-        
-%         (popGens(l,:)*B')
-%         (popVols(l)<=maxVol)
+
+        % Update fitness of Loser
         popFits(l) = (popGens(l,:)*B')*(popVols(l)<=maxVol);
-                
-        fitRec(:,g+1) = popFits;       
+
+        % Update fitness record
+        fitRec(:,g) = popFits;
     end
 
-    
+
     [winner,winnerInd] = max(popFits);
-    
+
 end
