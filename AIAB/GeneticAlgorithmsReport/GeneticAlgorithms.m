9380effaa1ccf048de6d1bcd4080324c0c64be22
David Attlee <da284@sussex.ac.uk>
2017-03-10 22:10:22 +0000

    Add length 20 genotype encoding

--- AIAB/GeneticAlgorithmsReport/GeneticAlgorithms.m
+++ AIAB/GeneticAlgorithmsReport/GeneticAlgorithms.m
@@ -1,11 +1,19 @@
-% % % 
-% % % The Items
-% % % 
-% 
-% %%% Small Number of genes
+%% 
+% % The Items
+% % 
+
+%%% Small Number of genes
 % MAX_VOLUME = 20; 
 % B = [5 6 1 9 2 8 4 3 7 10]; % Benefits
 % V = [3 2 4 5 8 9 10 1 6 7]; % Volumes
+% 
+% 
+% MAX_VOLUME = 50;
+% %benefit
+% B = [6    19     7     1    13    17     3    18    16    20     5    15    12    11     9     2    14     4    10     8];
+% 
+% %volume
+% V = [10     5    15    18     1    17    12    19    16     4     9    20    13     7     3     2     6    11     8    14];
 
 MAX_VOLUME = 50; 
 % Benefits
@@ -13,9 +21,9 @@ B = [3    54    55    32    50    26    70     7    45     1    40    28    46  
 % Volumes
 V = [  6     8     5    10     8     6     7    14    11     9    14     6    12    12    10     6    10     7     6     7    13     5     7     5     9     5    13     6     5     8     9     6    14     8     7     5     7     5    10    12    11     5     5    12    14    10     6    13 8     7    12     5     5    11    11    10    12    12    12     7    11    10     8     5    12     8    11    12     6     6    10     9    13    12    12     5     5     5    12    14    11     6    12     6     6    11     8    11    12    10    12     7    12    14    13     5 8     8    11    10];
 
-%%% Hillclimber GA
+%% Hillclimber GA
 pop = 100; % Population size
-gen = 100000; % No. Generations
+gen = 1000; % No. Generations
 generate = 1; % 1, to randomly generate population genomes, 0 to set to nothing
 mut = 1;
 
@@ -33,8 +41,8 @@ hold off;
 hillFile = sprintf('HillclimberFitnessIG%dP%dM%d',length(B),pop,HILLCLIMBER_MAX);
 print(hillFile,'-dpng')
 
-%%% Microbial GA
-gen = 100000;
+%% Microbial GA
+gen = 10000;
 mut = 1; % Mutation proportion 1 would be 1/length(B)
 local = 5;
 cross = 0.5;
